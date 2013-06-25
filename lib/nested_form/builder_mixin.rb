406b69f18adbaf8516d95c8f55e9bd073d866e5e
Jordan Hatch <jordan@jordanh.net>
2013-06-25 10:57:09 +0000

    Add a wrapper_class option

--- lib/nested_form/builder_mixin.rb
+++ lib/nested_form/builder_mixin.rb
@@ -90,6 +90,7 @@ module NestedForm
     def fields_for_nested_model(name, object, options, block)
       classes = 'fields'
       classes << ' marked_for_destruction' if object.respond_to?(:marked_for_destruction?) && object.marked_for_destruction?
+      classes << ' '+ options[:wrapper_class] if options[:wrapper_class].present?
 
       perform_wrap   = options.fetch(:nested_wrapper, true)
       perform_wrap &&= options[:wrapper] != false # wrap even if nil
--- spec/nested_form/builder_spec.rb
+++ spec/nested_form/builder_spec.rb
@@ -85,6 +85,18 @@ require "spec_helper"
 
           fields.should == '<div class="fields">Task</div><div class="fields">Task</div>'
         end
+
+        it "wraps nested fields each in a div with additional classes" do
+          2.times { project.tasks.build }
+
+          fields = if subject.is_a?(NestedForm::SimpleBuilder)
+            subject.simple_fields_for(:tasks, :wrapper_class => "foo") { "Task" }
+          else
+            subject.fields_for(:tasks, :wrapper_class => "foo") { "Task" }
+          end
+
+          fields.should == '<div class="fields foo">Task</div><div class="fields foo">Task</div>'
+        end
       end
 
       it "wraps nested fields marked for destruction with an additional class" do
